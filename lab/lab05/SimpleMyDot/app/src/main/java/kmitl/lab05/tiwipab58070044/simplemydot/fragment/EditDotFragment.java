46a5683434ad5c3f7a470384c8c4bfef9779bade
tiwipabmin <tiwipabmin@gmail.com>
2017-09-17 18:02:47 +0700

    fbtn_cancel set OnClickListener for close EditDotFragment

--- lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab05/tiwipab58070044/simplemydot/fragment/EditDotFragment.java
+++ lab/lab05/SimpleMyDot/app/src/main/java/kmitl/lab05/tiwipab58070044/simplemydot/fragment/EditDotFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import info.hoang8f.widget.FButton;
 import kmitl.lab05.tiwipab58070044.simplemydot.R;
 import kmitl.lab05.tiwipab58070044.simplemydot.model.Dot;
 
@@ -21,6 +22,8 @@ public class EditDotFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public FButton fbtn_ok = null, fbtn_cancel = null;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +33,22 @@ public class EditDotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_dot, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_edit_dot, container, false);
+
+        fbtn_ok = (FButton) rootView.findViewById(R.id.fbtn_ok);
+        fbtn_cancel = (FButton) rootView.findViewById(R.id.fbtn_cancel);
+
+        fbtn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .remove(EditDotFragment.this)
+                        .commit();
+            }
+        });
+
+        return rootView;
     }
 
     public static EditDotFragment newInstance(Dot dot) {
