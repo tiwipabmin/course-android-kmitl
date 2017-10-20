ec91fee3cf5ac5a872f8f28b49eb76bb4cedfa72
Student <student@LAB308-22.it.kmitl.ac.th>
2017-10-20 11:51:36 +0700

    add workshop

---
+++ workshop/Espresso/app/src/androidTest/java/kmitl/it/tiwipabmin58070044/espresso/MainActivityTest.java
@@ -0,0 +1,39 @@
+package kmitl.it.tiwipabmin58070044.espresso;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void showHelloWorldSuccessTest() {
+        onView(withText("Hello World!")).check(matches(withText("Hello World!")));
+
+    }
+
+}
