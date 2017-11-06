ecc6f4e367e226ce4b70fc082ae760b4587ac5b8
tiwipabmin <tiwipabmin@gmail.com>
2017-11-06 22:39:37 +0700

    create lab09 and arranged layout

---
+++ lab/lab09/moneyflow/app/src/main/java/kmitl/it/tiwipabmin58070044/moneyflow/MainActivity.java
@@ -0,0 +1,13 @@
+package kmitl.it.tiwipabmin58070044.moneyflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
